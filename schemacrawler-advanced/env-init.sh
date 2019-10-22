204e3b365a8082f8eeb14c9f6c52bd340699b4df
Sualeh Fatehi <sualeh@hotmail.com>
2019-10-22 23:47:38 +0530

    Change permissions on shared directory

--- schemacrawler-advanced/env-init.sh
+++ schemacrawler-advanced/env-init.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Setup a shared directory which will be mounted on the Docker container
+# and make the "users" group the owner
+adduser root users
 mkdir /share
+chown :100 /share
+chmod 777 /share
+chmod g+s /share
 
+# Run Docker container
 docker run \
 -v $(pwd):/share \
---user $(id -u):$(id -g) \
 --rm -i -t \
 --entrypoint=/bin/bash \
 schemacrawler/schemacrawler
