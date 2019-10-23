f1049708ba7192c7e4012356bedcf9663f1056f9
Sualeh Fatehi <sualeh@hotmail.com>
2019-10-23 19:42:34 +0530

    Use /root for Docker mount point

--- schemacrawler-advanced/env-init.sh
+++ schemacrawler-advanced/env-init.sh
@@ -3,14 +3,13 @@
 # Setup a shared directory which will be mounted on the Docker container
 # and make the "users" group the owner
 adduser root users
-mkdir /root/share
-chown :100 /root/share
-chmod 777 /root/share
-chmod g+s /root/share
+chown :100 /root
+chmod 777 /root
+chmod g+s /root
 
 # Run Docker container
 docker run \
--v /root/share:/home/schcrwlr/share \
+-v /root:/home/schcrwlr/share \
 --rm -i -t \
 --entrypoint=/bin/bash \
 schemacrawler/schemacrawler
