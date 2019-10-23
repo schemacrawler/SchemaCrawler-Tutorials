3a60aa9eabd25ea93732b94f511038b2faf368f4
Sualeh Fatehi <sualeh@hotmail.com>
2019-10-23 14:14:49 +0530

    Update env-init.sh

--- schemacrawler-advanced/env-init.sh
+++ schemacrawler-advanced/env-init.sh
@@ -3,14 +3,14 @@
 # Setup a shared directory which will be mounted on the Docker container
 # and make the "users" group the owner
 adduser root users
-mkdir /share
-chown :100 /share
-chmod 777 /share
-chmod g+s /share
+mkdir /root/share
+chown :100 /root/share
+chmod 777 /root/share
+chmod g+s /root/share
 
 # Run Docker container
 docker run \
--v $(pwd):/share \
+-v /root/share:/home/schcrwlr/share \
 --rm -i -t \
 --entrypoint=/bin/bash \
 schemacrawler/schemacrawler
