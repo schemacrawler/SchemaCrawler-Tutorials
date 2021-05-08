bf8d03e49a0ad15ae167eb3805eb84881aacd82e
Sualeh Fatehi <sualeh@hotmail.com>
2021-05-08 10:33:49 -0400

    v16.14.6

--- schemacrawler-output-files/scripts/env-init.sh
+++ schemacrawler-output-files/scripts/env-init.sh
@@ -18,4 +18,4 @@ docker run \
 --name schemacrawler \
 --rm -i -t \
 --entrypoint=/bin/bash \
-schemacrawler/schemacrawler:v16.14.5
+schemacrawler/schemacrawler:v16.14.6
--- schemacrawler/scripts/env-init.sh
+++ schemacrawler/scripts/env-init.sh
@@ -13,4 +13,4 @@ docker run \
 --name schemacrawler \
 --rm -i -t \
 --entrypoint=/bin/bash \
-schemacrawler/schemacrawler:v16.14.5
+schemacrawler/schemacrawler:v16.14.6
