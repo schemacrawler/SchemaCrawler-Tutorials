3dbb5a0973c094e26e37e4fba318e2119f77a47a
Sualeh Fatehi <sualeh@hotmail.com>
2021-09-29 21:50:31 -0400

    Update install script

--- schemacrawler-examples/scripts/file-browser-init.sh
+++ schemacrawler-examples/scripts/file-browser-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Start the web server in the background
-apt-get install -qq -y python-twisted-core &
+sudo apt-get -q -y install python-twisted-core
 twistd -n web -p 80 --path ~/schemacrawler-16.15.7-distribution/examples > ~/webserver.log 2>&1 &
