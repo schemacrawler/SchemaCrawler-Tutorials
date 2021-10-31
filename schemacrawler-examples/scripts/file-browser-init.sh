5190961da8b37f6347463ce78dec9b0d43c2fb96
Sualeh Fatehi <sualeh@hotmail.com>
2021-10-30 23:23:06 -0400

    v16.15.11

--- schemacrawler-examples/scripts/file-browser-init.sh
+++ schemacrawler-examples/scripts/file-browser-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start the web server in the background
-python3 -m http.server 80 --directory ~/schemacrawler-16.15.10-distribution/examples > ~/webserver.log 2>&1 &
+python3 -m http.server 80 --directory ~/schemacrawler-16.15.11-distribution/examples > ~/webserver.log 2>&1 &
--- schemacrawler-exploration/scripts/env-init.sh
+++ schemacrawler-exploration/scripts/env-init.sh
@@ -17,4 +17,4 @@ docker run \
 --name schemacrawler \
 --rm -i -t \
 --entrypoint=/bin/bash \
-schemacrawler/schemacrawler:v16.15.10
+schemacrawler/schemacrawler:v16.15.11
