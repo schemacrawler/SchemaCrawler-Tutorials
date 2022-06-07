f1c88231889d745fe5e5f4c10747a0a9fc708cf2
Sualeh Fatehi <sualeh@hotmail.com>
2022-06-07 16:03:53 -0400

    v16.16.16

--- schemacrawler-examples/scripts/file-browser-init.sh
+++ schemacrawler-examples/scripts/file-browser-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start the web server in the background
-python3 -m http.server 80 --directory ~/schemacrawler-16.16.15-distribution/examples > ~/webserver.log 2>&1 &
+python3 -m http.server 80 --directory ~/schemacrawler-16.16.16-distribution/examples > ~/webserver.log 2>&1 &
