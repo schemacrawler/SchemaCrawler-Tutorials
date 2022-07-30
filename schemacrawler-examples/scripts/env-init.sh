cefdb2e9a5bfcf1ecbd3945ae393e6f266a10a41
Sualeh Fatehi <sualeh@hotmail.com>
2022-07-29 21:12:32 -0400

    v16.17.2

--- schemacrawler-examples/scripts/env-init.sh
+++ schemacrawler-examples/scripts/env-init.sh
@@ -16,5 +16,5 @@ docker run \
 -v /root/share:/home/schcrwlr/share \
 --name schemacrawler \
 --rm -i -t \
-schemacrawler/schemacrawler:v16.17.1 \
+schemacrawler/schemacrawler:v16.17.2 \
 /bin/sh -l
