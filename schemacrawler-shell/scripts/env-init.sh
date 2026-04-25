cff095cacd85627378e0e1340548673a0575dbce
Sualeh Fatehi <sualeh@hotmail.com>
2026-04-24 23:26:59 -0400

    v17.10.2

--- schemacrawler-shell/scripts/env-init.sh
+++ schemacrawler-shell/scripts/env-init.sh
@@ -15,5 +15,5 @@ docker run \
 -v /root/share:/home/schcrwlr/share \
 --name schemacrawler \
 --rm -i -t \
-schemacrawler/schemacrawler:v17.10.1 \
+schemacrawler/schemacrawler:v17.10.2 \
 bash
--- schemacrawler/scripts/env-init.sh
+++ schemacrawler/scripts/env-init.sh
@@ -16,5 +16,5 @@ docker run \
 -v /root/share:/home/schcrwlr/share \
 --name schemacrawler \
 --rm -i -t \
-schemacrawler/schemacrawler:v17.10.1 \
+schemacrawler/schemacrawler:v17.10.2 \
 bash
